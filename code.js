5d7213a6fdf3901192a7ae8669fd1f82d37dc3f0
Jodie Sauer <jodie.sauer@docc.techstarter.de>
2022-11-28 13:06:18 +0100

    compareDates() um Parameter erweitert.

--- code.js
+++ code.js
@@ -1,39 +1,30 @@
-function onButtonClick(){
-    window.alert("Das hättest du wohl gerne ;-)");
+/* //Funktion, die 24 Türchen im Kalender erstellt (in random order)
+function test(){
+    const tuerchenX = <a onmouseover="compareDates('tuerID')" href="https://en.wikipedia.org/wiki/Special:Random" target="_blank">
+    <article class="tuerchen" id="tuerID">1</article></a>;
+    const node = document.createElement('div');
+    const textnode = document.create(tuerchenX);
+    node.appendChild(textnode);
 }
-
-function compareDates(){
+ */
+//Funktion, die einen Datumsvergleich (leider nur die Zahlen, nicht richtige Datumsangaben) anstellt.
+function compareDates(tuerID){
 
 let dateSim = document.getElementById("dateSim").value;
-let dateTuer = document.getElementById("dateTuer").textContent;
+let dateTuer = document.getElementById(tuerID).textContent;
+//Umwandlung des Strings in einen Integer Zahlenwert, da es sonst Probleme gibt beim Zahlenvergleich.
+dateTuer = parseInt(dateTuer);
 
     if (dateSim>=dateTuer) {
-        console.log("Ja, du darfst");
+        console.log("Ja, du darfst.");
     }
     else {
-        console.log("Zu früh dran, Schatzi 1!")
-        window.alert("Zu früh dran, Schatzi!");
+        console.log("Zu früh dran, Schatzi!")
+        window.alert("Du bist leider zu früh dran. Habe noch ein wenig Geduld :)");
     }
 }        
 
-function compareDates24(){
-
-    let dateSim = document.getElementById("dateSim").value;
-    let dateTuer = document.getElementById("dateTuer24").textContent;
-    
-    if (dateSim>=dateTuer) {
-        console.log("Ja, du darfst");
-    }
-    else {
-        console.log("Zu früh dran, Schatzi 24!")
-        window.alert("Zu früh dran, Schatzi!");
-    }
-}      
-/* function compareDates() {
-    if (dateSim<dateTuer) {
-        console.log("Zu früh dran!");
-    }
-    else {
-        console.log("Ja, du darfst");
-    }        
-} */
+// Was passiert beim Klick auf den Burger-Button
+function onButtonClick(){
+    window.alert("Das hättest du wohl gerne ;-)");
+}
